e0211323b446cfc4dd66e566d17e3f8e5a323187
Sindia Choolackal <sindiapc@gmail.com>
2019-04-26 08:46:18 +0530

    address change commit

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace aspnet_get_started.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. changed from code";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
